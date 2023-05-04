8310604267c07370962bbad34894a73f50809805
Elder <elderfavsto@gmail.com>
2023-05-04 14:54:46 -0300

    Date

--- _4-  Comportamento de memorias arrays listas/List/src/Main.java
+++ _4-  Comportamento de memorias arrays listas/List/src/Main.java
@@ -31,7 +31,7 @@ public class Main {
       System.out.println(result);
     }
 
-    String name = list.stream().filter(x -> x.charAt(0) == "J").findFirst().orElse(null);
+    // name = list.stream().filter(x -> x.charAt(0) == "J").findFirst().orElse(null);
   }
 }
 
