afbf8c9bbaf6f1624869d3a4aadb944c5966db30
Elder <elderfavsto@gmail.com>
2023-06-12 13:22:33 -0300

    generics

--- _11 - Interfaces/exercicio-interface/Default-methods/src/services/BrazilInterestService.java
+++ _11 - Interfaces/exercicio-interface/Default-methods/src/services/BrazilInterestService.java
@@ -5,7 +5,6 @@ import java.security.InvalidParameterException;
 public class BrazilInterestService implements InterestService {
   private double interestRate;
 
-
   public BrazilInterestService(double interestRate) {
     this.interestRate = interestRate;
   }
